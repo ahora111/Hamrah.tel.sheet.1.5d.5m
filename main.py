9d2a292db8ed2b01f90dec000d8b4a598acaf4c4
ahora111 <nimbuz72@gmail.com>
2025-03-29 14:24:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,9 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def get_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--incognito")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--mute-audio")
-    options.add_argument("--disable-notifications")
     service = Service()
     driver = webdriver.Chrome(service=service, options=options)
     return driver
@@ -95,7 +90,7 @@ def send_to_telegram(models, brands):
                 print("✅ پیام به تلگرام ارسال شد.")
             else:
                 print(f"❌ خطا در ارسال پیام به تلگرام: {response.text}")
-            time.sleep(1)
+            time.sleep(1)  # برای جلوگیری از Flood Limit
     except Exception as e:
         print(f"❌ خطا در ارسال به تلگرام: {e}")
 
